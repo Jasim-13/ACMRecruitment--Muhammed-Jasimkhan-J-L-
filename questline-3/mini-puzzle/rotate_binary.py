1aa014cdd1b4b5dfbf402843be81273e47970778
Jasimkhan <imxghoul@gmail.com>
2025-09-10 23:39:33 +0530

    Update rotate_binary.py

--- questline-3/mini-puzzle/rotate_binary.py
+++ questline-3/mini-puzzle/rotate_binary.py
@@ -1,1 +1,9 @@
+def rotate_binary_to_decimal(A, k):
+    n = len(A)
+    k = k % n
+    rotated = A[-k:] + A[:-k]
+    decimal_value = int(rotated, 2)
+    return decimal_value
+print(rotate_binary_to_decimal("1011", 1))  
+print(rotate_binary_to_decimal("1011", 2))
 
